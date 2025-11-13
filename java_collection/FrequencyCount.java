45f97c89134e98026e173b34a712cc3688f7960c
Anshu Kumar <anshu2587k@gmail.com>
2025-11-13 15:40:10 +0530

    Add files via upload

---
+++ java_collection/FrequencyCount.java
@@ -0,0 +1,13 @@
+package java_collection;
+
+import java.util.*;
+
+public class FrequencyCount {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("apple", "banana", "apple", "orange");
+        Map<String, Integer> freq = new HashMap<>();
+        for (String s : list)
+            freq.put(s, freq.getOrDefault(s, 0) + 1);
+        System.out.println(freq);
+    }
+}
